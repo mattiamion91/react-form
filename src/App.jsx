91e83db6b8f4b84457f598b7a5006c209b8332f1
Mattia Mion <mattia.mion@gmail.com>
2026-01-22 15:24:55 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.css';
+
+function App() {
+
+  return (<main className='container'>
+    <div className="row">
+      <div className="col">
+        <ul class="list-group">
+          <li class="list-group-item">An item</li>
+          <li class="list-group-item">A second item</li>
+          <li class="list-group-item">A third item</li>
+          <li class="list-group-item">A fourth item</li>
+          <li class="list-group-item">And a fifth one</li>
+        </ul>
+      </div>
+    </div>
+
+  </main>
+
+  )
+}
+
+export default App
